37e3723bc24ab2888568307e28f36f751b433538
lucdevasconcellosbr <lucdevasconcellosbr@gmail.com>
2025-08-27 11:21:52 -0300

    Alterações

--- Aula01_OlaMundo/OlaMundo.java
+++ Aula01_OlaMundo/OlaMundo.java
@@ -1,6 +1,5 @@
 package Aula01_OlaMundo;
-import javax.swing.JOptionPane;
-class OlaMundo {
+ public class OlaMundo {
 
 	public static void main(String[] args){
 		System.out.println("0la, Mundo!");
